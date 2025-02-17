6d819ab4b4c65a388e1ae07b21127fa515efa502
yadhu <yadhusnair009@gmail.com>
2025-02-17 12:28:48 +0530

    python_basics

---
+++ python_basics.py
@@ -0,0 +1,12 @@
+int 0
+str "tim" , "yadhu"
+bool    True / False 
+float 0.23 , 0.123
+variable - x = 5
+argument  - inside print statement eg - print("argument")
+
+operator = + - / *
+exponent = **
+integer division = //
+modulus = % (remainder of the answe)
+input() = will always be string - convert to int() if needed
---
+++ python_learn_updates.py
@@ -0,0 +1,8 @@
+print("what is your name?")
+name = input()
+print("hello" ,name)
+
+num1 = 45
+num2 = 4
+num3 = num1 ** num2
+print(num3)
